e105574b0e9b3202aea323bd37836229aa9deaa1
90roma <roman_grishin@hotmail.com>
2021-04-28 18:12:57 +0300

    added sidebar and header/ added icon

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import {Button} from "../Button";
-import sideBarStrIcon1 from '../Avatar/sideBarStr-Icon1.png';
-import btnChatIcon from '../Avatar/btn-chat-icon.png';
-import "./style.css";
 import {Avatar} from "../Avatar";
+import sideBarStrIcon from '../Avatar/sideBarStr-Icon.png';
+import btnChatIcon from '../Avatar/btn-chat-icon.png';
 import avatarDev from "../Avatar/avatar-dev.png";
 import avatarMark from "../Avatar/avatar-mark.png";
 import avatarDes from "../Avatar/avatar-des.png";
+import "./style.css";
 
 const Header = ({title}) => {
     return (
         <div className="Header">
             <div className="Header__wrapper">
-                <img className="Header__logo" src={sideBarStrIcon1} alt="header logo"/>
+                <img className="Header__logo" src={sideBarStrIcon} alt="header logo"/>
                 <h1 className="Header__title">{title}</h1>
                 <Button/>
                 <div className="Header__info">
